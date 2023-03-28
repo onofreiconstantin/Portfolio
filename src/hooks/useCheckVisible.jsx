d3b6e4e09da626ff3bd77737b0fd448b05204872
CONSTANTIN ONOFREI <constantin.onofrei22@gmail.com>
2023-03-28 21:37:00 +0300

    [FIX] optimized images

--- src/hooks/useCheckVisible.jsx
+++ src/hooks/useCheckVisible.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const useCheckVisible = (parentRef) => {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const observer = new IntersectionObserver(([entry]) => {
+      console.log("se seteaza true");
       setIsVisible(entry.isIntersecting);
     });
 
