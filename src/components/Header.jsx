9165adcc601866503c608d746836002a029573ab
CONSTANTIN ONOFREI <constantin.onofrei22@gmail.com>
2023-03-29 20:23:52 +0300

    [FIX] removed an animation that caused a buggy display

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -49,7 +49,7 @@ const Header = () => {
           })}
         </div>
       </div>
-      <div className="header__image rightToLeft">
+      <div className="header__image">
         <img
           src={portfolioImage}
           alt="Portfolio Image"
